c319a4a1c87d08d91f06c01d7b6cfccca9b35af9
Alejandro Perez Moreno <alu0101132907@ull.edu.es>
2020-02-10 10:09:28 +0000

    Updated concepts (less generic, more functional)

--- concepts.hpp
+++ concepts.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-template <typename T, template <size_t> typename Temp>
-struct is_specialization: std::false_type {};
+template <typename>
+struct IsDataSpecialization: std::false_type {};
 
-template <template <size_t> typename Temp, size_t S>
-struct is_specialization<Temp<S>, Temp>: std::true_type {};
+template <size_t S>
+struct IsDataSpecialization<Data<S>>: std::true_type {};
 
